ebaf34457671c4903727afa6a3dd6e025a43e1e6
Alan Russell <ajrussellaudio@gmail.com>
2016-10-03 21:23:31 +0100

    api tasks done

---
+++ client/src/models/filmApi.js
@@ -0,0 +1,39 @@
+var films = require('./films')();
+
+var FilmApi = function( app ) {
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '../../build/index.html'));
+  });
+
+  app.get('/films/:id', function( req, res ) {
+    res.json({ film: films[req.params.id] });
+  });
+
+  app.get('/films', function( req, res ) {
+    res.json( films );
+  });
+
+  app.delete('/films/:id', function( req, res ) {
+    films.splice(req.params.id, 1);
+    res.json( films );
+  });
+
+  app.put('/films/:id', function( req, res ) {
+    films[req.params.id] = req.body.film;
+    res.json({ film: films[req.params.id] });
+  });
+
+  app.post('/films', function( req, res ) {
+    films.push( req.body.film );
+    res.json( films );
+  });
+
+  app.post('/films/:id/reviews', function( req, res ) {
+    films[req.params.id].reviews.push( req.body.review );
+    res.json({ film: films[req.params.id] })
+  })
+
+}
+
+module.exports = FilmApi;
